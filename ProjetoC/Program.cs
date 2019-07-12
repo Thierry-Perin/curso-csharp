bb492e673a0bcf1cfc46dc7912cf306971a92287
Thierry Perin <thierryperin@hotmail.com>
2019-07-12 17:11:29 -0300

    Aula usando gitbash

---
+++ ProjetoC/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+
+            Console.WriteLine("O curso A possui quantos alunos A?");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os codigos dos alunos do curso A:");
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                A.Add(x);
+            }
+
+            Console.WriteLine("Quantos alunos possui o curso B");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso B: ");
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                B.Add(x);
+            }
+            Console.Write("Quantos alunos possui o curso C? ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso C: ");
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                C.Add(x);
+            }
+            HashSet<int> Novo = new HashSet<int>();
+            Novo.UnionWith(A);
+            Novo.UnionWith(B);
+            Novo.UnionWith(C);
+            Console.WriteLine("Total de Alunos: " + Novo.Count);
+            Console.ReadLine();
+
+        }
+    }
+}
