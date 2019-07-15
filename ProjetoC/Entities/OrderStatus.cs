415abf1d129e9451e23b721af19c89caf0736bef
Thierry Perin <thierryperin@hotmail.com>
2019-07-15 16:24:17 -0300

    122 Exercicio Enumeração e Composição

---
+++ ProjetoC/Entities/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities
+{
+    enum OrderStatus : int
+    {
+        PendinPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
